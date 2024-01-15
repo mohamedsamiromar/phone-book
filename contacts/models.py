9556af59090f9b5e7c2fb41c347375c08d3744ed
Mohamed Samir <m.samir@munjiz.sa>
2024-01-15 20:20:52 +0200

    enhc: change the db design to make contacts have a many phone number

--- contacts/models.py
+++ contacts/models.py
@@ -5,8 +5,6 @@ from account.models import BaseModel, User
 class Contact(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_contact', null=True)
     name = models.CharField(max_length=100)
-    phone_number1 = models.CharField(max_length=15, null=True)
-    phone_number2 = models.CharField(max_length=15, null=True)
     telephone = models.CharField(max_length=10, null=True)
     email = models.EmailField(max_length=100, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
@@ -14,3 +12,11 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class PhoneNumber(models.Model):
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, null=True)
+    phone_number = models.CharField(max_length=15, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.contact}_{self.phone_number}"
